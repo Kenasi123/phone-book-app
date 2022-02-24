79a19f6e111b2f400a2abb0180f26dbf2e0c3c1f
KenasiaTurner <kenasi123@gmail.com>
2022-02-24 07:43:26 -0500

    Looks up a contact

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,4 @@
+hasQuit = False
 phonebook = {}
 
 menu = """" 
@@ -10,13 +11,22 @@ Electronic Phone Book
 5. Quit
 
 """ 
-print(menu)
 
-select_option = input("What do you want to do (1-5?)")
+while not(hasQuit):
+    print(menu)
 
-if select_option == "2": 
-    name = input("What is the contacts name?") 
-    phone_number = input("What is the contacts phone number?") 
-    phonebook[name] = phone_number 
-    print("Contacts added ssuccesfully")
-    print(menu) 
+    select_option = input("What do you want to do (1-5?)")
+
+    if select_option == "2": 
+        name = input("What is the contacts name?") 
+        phone_number = input("What is the contacts phone number?") 
+        phonebook[name] = phone_number 
+        print("Contacts added ssuccesfully")
+    elif select_option == "1":
+        name = input("What contact's number would you like?") 
+        if phonebook.get(name) == None: 
+             print("There's no contact with that name...")
+        else: 
+             print("Here's their number: ", phonebook[name]) 
+    elif select_option == "5": 
+        hasQuit = True 
